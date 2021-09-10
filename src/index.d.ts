45feb846279b622172be5bbc2694556a1bbb1868
Jiri Hoffmann <hoffik36@gmail.com>
2021-09-10 13:57:28 -0400

    Update index.d.ts

--- src/index.d.ts
+++ src/index.d.ts
@@ -6,16 +6,19 @@
 | EEA | 1 | Geography appears as in EEA for debug devices.  |
 | NOT_EEA | 2 | Geography appears as not in EEA for debug devices.  |
  */
-type DebugGeography = {
+type DebugGeographyOptions = {
     DISABLED: 0;
     EEA: 1;
     NOT_EEA: 2
-}
+};
+
+type DebugGeographyOptions = 0 | 1 | 2;
+
 
 type ConsentInfoConfig = {
-    debugGeography: DebugGeography,
+    debugGeography: DebugGeographyOptions,
     testDeviceIds: Array<String>,
-}
+};
 
 /**
 | Name  |   Value  | Description |
@@ -25,27 +28,29 @@ type ConsentInfoConfig = {
 | REQUIRED | 2 | User consent required but not yet obtained.  |
 | OBTAINED | 3 | User consent obtained. Personalized vs non-personalized undefined. |
  */
-type ConsentStatus = {
+type ConsentStatusOptions = {
     UNKNOWN: 0,
     NOT_REQUIRED: 1
     REQUIRED: 2
     OBTAINED: 3
-}
+};
+
+type ConsentStatus = 0 | 1 | 2 | 3;
 
 type ConsentInfoUpdate = {
     consentStatus: ConsentStatus,
     isConsentFormAvailable: boolean,
     isRequestLocationInEeaOrUnknown: boolean,
-}
+};
 
 type ConsentFormResponse = {
     consentStatus: ConsentStatus,
-}
+};
 
 
 type UMP = {
-    CONSENT_STATUS: ConsentStatus,
-    DEBUG_GEOGRAPHY: DebugGeography,
+    CONSENT_STATUS: ConsentStatusOptions;
+    DEBUG_GEOGRAPHY: DebugGeography;
     /**
      * Returns the consent information.
      */
@@ -60,18 +65,18 @@ type UMP = {
    * Shows the consent form and returns the updated consentStatus on close.
    */
     showConsentForm(): Promise<ConsentFormResponse>;
-}
+};
 
 declare module "react-native-ad-consent" {
     export const DebugGeography: DebugGeography;
 
     export const ConsentInfoConfig: ConsentInfoConfig;
 
-    export const ConsentStatus: ConsentStatus;
+    export const ConsentStatus: ConsentStatusOptions;
 
     export const ConsentInfoUpdate: ConsentInfoUpdate;
 
     export const ConsentFormResponse: ConsentFormResponse;
 
     export const UMP: UMP;
-}
+};
